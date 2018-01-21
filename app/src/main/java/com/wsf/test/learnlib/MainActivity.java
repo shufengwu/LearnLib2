0032eebcfbdbf788afebd8919a264ef304f928cb
shufengwu <sfwandroid@163.com>
2018-01-22 00:53:05 +0800

    data binding高级使用

--- app/src/main/java/com/wsf/test/learnlib/MainActivity.java
+++ app/src/main/java/com/wsf/test/learnlib/MainActivity.java
@@ -2,6 +2,8 @@ package com.wsf.test.learnlib;
 
 import android.database.DatabaseUtils;
 import android.databinding.DataBindingUtil;
+import android.databinding.ObservableArrayMap;
+import android.databinding.ObservableMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,21 +12,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.wsf.test.learnlib.databinding.ActivityMainBinding;
+import com.example.ContactItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     Button button;
     MainActivity mainActivity;
-    ActivityMainBinding activityMainBinding;
+    ContactItem activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainActivity = this;
         activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
-        final User user = new User("test","user");
+        /*final User user = new User("test","user");
+        user.setVisibile(true);
         activityMainBinding.setUser(user);
         activityMainBinding.setMainActivity(mainActivity);
         String content = "toastContent";
@@ -37,6 +41,44 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 user.setFirstName("w");
                 user.setLastName("sf");
+            }
+        });*/
+
+        /*final User user = new User();
+        user.firstName.set("test");
+        user.lastName.set("user");
+        user.visible.set(true);
+        activityMainBinding.setUser(user);
+        activityMainBinding.setMainActivity(mainActivity);
+        String content = "toastContent";
+        String longClickContent = "toastLongClickContent";
+        activityMainBinding.setContent(content);
+        activityMainBinding.setLongClickContent(longClickContent);
+        button = findViewById(R.id.test);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user.firstName.set("w");
+                user.lastName.set("sf");
+            }
+        });*/
+
+        final ObservableMap<String, Object> user = new ObservableArrayMap<>();
+        user.put("firstName", "test");
+        user.put("lastName", "user");
+        user.put("visible", true);
+        activityMainBinding.setUser(user);
+        activityMainBinding.setMainActivity(mainActivity);
+        String content = "toastContent";
+        String longClickContent = "toastLongClickContent";
+        activityMainBinding.setContent(content);
+        activityMainBinding.setLongClickContent(longClickContent);
+        button = findViewById(R.id.test);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user.put("firstName", "w");
+                user.put("lastName", "sf");
             }
         });
     }
--- app/src/main/java/com/wsf/test/learnlib/User.java
+++ app/src/main/java/com/wsf/test/learnlib/User.java
@@ -2,18 +2,20 @@ package com.wsf.test.learnlib;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
-
-import com.wsf.test.learnlib.databinding.ActivityMainBinding;
+import android.databinding.ObservableBoolean;
+import android.databinding.ObservableField;
 
 /**
  * Created by Shufeng.Wu on 2018/1/19.
  */
 
-public class User extends BaseObservable{
+/*public class User extends BaseObservable{
 
     private  String firstName;
 
     private  String lastName;
+
+    private boolean visible = true;
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
@@ -32,11 +34,26 @@ public class User extends BaseObservable{
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
-        notifyPropertyChanged(BR.firstName);
+        notifyPropertyChanged(com.wsf.test.learnlib.BR.firstName);
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-        notifyPropertyChanged(BR.lastName);
+        notifyPropertyChanged(com.wsf.test.learnlib.BR.lastName);
     }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+}*/
+
+public class User{
+
+    /*public final ObservableField<String> firstName = new ObservableField<>();
+    public final ObservableField<String> lastName = new ObservableField<>();
+    public final ObservableBoolean visible = new ObservableBoolean();*/
 }
